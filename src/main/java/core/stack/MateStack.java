a36b830ed26a99d6dce2f8b5ef316f2a91f6ccc0
Szymon Twardowski <szytwardowski@gmail.com>
2025-11-23 20:22:40 +0100

    Stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,62 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> top;
+    private int size;
+
+    public void push(T value) {
+        Node<T> newNode = new Node<>(value);
+        newNode.setNext(top);
+        top = newNode;
+        size++;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
     }
 
     public T peek() {
-        return null;
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+        return top.getValue();
     }
 
     public T pop() {
-        return null;
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+
+        T value = top.getValue();
+        top = top.getNext();
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
